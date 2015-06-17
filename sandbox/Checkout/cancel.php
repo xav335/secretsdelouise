6583f0b8f0938c889004316ad34a60e4b36ae2e7
Xavier Gonzalez <xavier@gonzalez.pm>
2015-06-17 09:20:45 +0200

    debut fdpf + facture

---
+++ sandbox/Checkout/cancel.php
@@ -0,0 +1,11 @@
+<?php include('../../Checkout/header.php'); ?>
+	<span class="span4">
+	</span>
+	<span class="span5">
+		<div class="hero-unit">
+		   You cancelled the order.
+		</div>
+	</span>
+	<span class="span3">
+	</span>
+<?php include('../../Checkout/footer.php'); ?>
