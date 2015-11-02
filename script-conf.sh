eb615b21e665072aac0ff3edd5f35b9701a088cd
root <root@vmprod1.iconeo.fr>
2015-11-02 11:27:05 +0100

    correction awstats

--- script-conf.sh
+++ script-conf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SERVERNAME=/home/web/lessecretsdelouise.com
+SERVERNAME=/var/www/lessecretsdelouise.com
 
-chown -R www-data.ftpgroup $SERVERNAME
+chown -R www-data.www-data $SERVERNAME
 chmod -R 755 $SERVERNAME
 chmod -R 777 $SERVERNAME/log/spy.log
 chmod -R 777 $SERVERNAME/admin/FileUpload/server/php/files
