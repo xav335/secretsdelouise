0f4c2728a029307542df4c583857d7da8c216e98
Xavier Gonzalez <xavier@gonzalez.pm>
2015-11-03 21:37:14 +0100

    mentions

--- mentions.php
+++ mentions.php
@@ -20,7 +20,71 @@ session_start();
 <!-- End Breadcrumb -->	
 	<!-- Actualités -->
 	<div class="row actualites">
-		<h1>Mentions légales</h1><br>
+		<h1>POLITIQUE DE CONFIDENTIALITE</h1>
+
+
+Le site « Les Secrets de Louise » est exploité par la société LSL e-services, société par actions simplifiée au capital de 1000 euros, dont le siège est à ARCACHON (33120) 12 bis, cours Héricart de Thury.
+
+Nous attachons une attention particulière à la protection de vos données personnelles et nous ne recueillons, traitons et utilisons vos données que conformément aux conditions décrites ci-dessous et dans le cadre des lois applicables à la protection des données.
+
+<h2>I - VOS DONNEES PERSONNELLES</h2>
+
+Une donnée personnelle correspond à toute information relative à une personne identifiée ou identifiable. Ce peut être par exemple votre nom, votre numéro de téléphone ou votre adresse postale ou votre e-mail.
+
+<h2>II – COLLECTE, TRAITEMENT ET UTILISATION DE DONNEES PERSONNELLES</h2>
+
+Le site « Les Secrets de Louise » ne collecte, traite ou n'utilise vos données personnelles qu'exclusivement dans le cadre décrit ci-dessous.
+
+<h3>A – Compte de membre « Les Secrets de Louise »</h3>
+
+Sur le site, vous avez la possibilité de créer un compte personnel pour rejoindre la communauté d'achat.
+Dans votre compte de membre, vous pouvez sauvegarder vos informations personnelles et donc faire des achats au sein de la communauté d'achat.
+
+Pour la création de votre compte personnel, le site « Les Secrets de Louise » aura besoin de votre adresse e-mail et d'un mot de passe choisi librement et, si nécessaire, de votre titre, de votre nom et prénom.
+
+L'adresse e-mail que vous fournirez servira également d'identificateur d'accès pour votre compte de membre. Vous recevrez gratuitement un e-mail confirmant la réussite de votre inscription. Vous pouvez mettre à jour vos données à tout moment dans la section personnelle de votre compte de membre.
+
+Si vous êtes membre de la communauté Facebook, vous pourrez également utiliser le service « Facebook connexion pages d'accueil » pour vous connecter à notre site.
+
+<h3>B -  Commandes</h3>
+
+Pour traiter les commandes, le site « Les Secrets de Louise » aura besoin de votre titre, de votre nom et de votre prénom, de votre adresse ainsi que de votre numéro de téléphone. Ces données sont transmises au cours du processus de commande au cas où elles n'auraient pas encore été enregistrées dans votre compte de membre. Vous avez la possibilité de spécifier une adresse de livraison différente de l'adresse de facturation.
+
+Après avoir sélectionné un fournisseur de service de paiement, vous serez invité à entrer les données nécessaires pour autoriser l'usage de ce fournisseur de services de paiement. Ces données de paiement sont envoyées directement au fournisseur de services de paiement et ne sont en aucune manière sauvegardées par nous.
+
+Nous sauvegardons uniquement les données utilisées pour le processus de facturation et l'adresse de facturation que vous aurez spécifié dans votre compte de membre de manière à ce que vous n'ayez plus à les saisir une nouvelle fois lors d'un prochain achat. Vous pourrez modifier ces données à tout moment.
+
+<h3>C – FORMULAIRE DE CONTACT</h3>
+
+Vous pouvez contacter « Les Secrets de Louise » directement en utilisant le formulaire de contact disponible sur le site.
+Nous ne recueillons, n'utilisons et ne traitons les informations que vous avez fournies dans le formulaire de contact que pour traiter votre demande.
+
+<h3>D – LETTRE D'INFORMATION SUR LE SITE</h3>
+
+Dans la mesure où vous l'aurez accepté, « Les Secrets de Louise » utilisera l'adresse e-mail que vous avez spécifié préalablement lors de la configuration de votre compte pour vous transmettre par e-mail des informations sur les ventes en cours.
+
+Vous pouvez gérer les paramètres de la lettre d'information dans la section personnelle de votre compte de membre. Vous pouvez naturellement vous désabonner de la lettre d'information dans la section personnelle de votre compte de membre ou par le biais du formulaire de contact.
+
+<h3>E – UTILISATION DE PRESTATAIRES DE SERVICES LOGISTIQUES</h3>
+
+En envoyant votre commande, « Les Secrets de Louise » met vos renseignements personnels à la disposition de prestataires de services logistiques qui traitent ces données pour notre compte afin d'exécuter votre commande conformément à nos instructions et aux mesures de sécurité nécessaires.
+
+<h2>III – DROIT D'ACCES ET DE RECTIFICATION</h2>
+
+Vous disposez d'un droit d'accès gratuit aux données conservées vous concernant.
+
+Vous pouvez révoquer le consentement que vous avez donné à la collecte, au traitement et à l'utilisation de données à tout moment. Cette révocation prendra effet à la suite de la réception de votre demande.
+
+Nous serons heureux de répondre à toute question concernant la confidentialité et le traitement de vos données personnelles.
+
+Vous pouvez à tout moment nous contacter à l'adresse e-mail : service@lessecretsdelouise.com
+
+De plus, vous pouvez également nous joindre à l'adresse de contact indiqué dans les coordonnées de la société.
+
+Date de prise d'effet : 15 novembre 2015
+
+
+<h3>LSL e-services.</h3>
 		
 		
 		
